71ae80791615d51585d65414e3d039fd50e66b49
berkayakcay <berkayakcay.m@gmail.com>
2017-06-26 01:41:25 +0300

    ERPin.Test project

---
+++ ERPin.Test/EntityTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ERPin.Models;
+using ERPin.Repositories;
+using ERPin.UnitOfWork;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ERPin.Test
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestClass]
+    public class EntityTest
+    {
+
+        private ERPinDbContext _dbContext;
+        private IUnitOfWork _unitOfWork;
+        private IRepository<CurrAcc> _currAccRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _dbContext = new ERPinDbContext();
+
+            _unitOfWork = new EfUnitOfWork(_dbContext);
+            _currAccRepository = _unitOfWork.GetRepository<CurrAcc>();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _dbContext = null;
+            _unitOfWork.Dispose();
+        }
+
+        [TestMethod]
+        public void AddCurrAcc()
+        {
+
+        }
+    }
+}
---
+++ ERPin.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ERPin.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ERPin.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4aa95a0-b105-48de-8eb2-3da99bfb5beb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
