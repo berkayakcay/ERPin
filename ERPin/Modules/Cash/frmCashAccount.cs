c03cbc2633b6e18b5592428f0c2cfc27e9cc9621
berkayakcay <berkayakcay.m@gmail.com>
2017-06-29 15:29:41 +0300

    Cash Account

--- ERPin/Modules/Cash/frmCashAccount.cs
+++ ERPin/Modules/Cash/frmCashAccount.cs
@@ -42,46 +42,27 @@ namespace ERPin.Modules.Cash
 
         private void frmCashAccount_Load(object sender, EventArgs e)
         {
-            ListRecord();
-            txtCashAccCode.Text = _numbers.LastCashAccCode();
+
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (_edit && _selectionId > 0 && _messages.Update() == DialogResult.Yes)
-            {
-                UpdateRecord();
-            }
-            else
-            {
-                CreateRecord();
-            }
+
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (_edit && _selectionId > 0 && _messages.Delete() == DialogResult.Yes)
-            {
-                DeleteRecord();
-            }
+
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            this.Close();
-        }
 
-        private void gridView1_DoubleClick(object sender, EventArgs e)
-        {
-            SelectRecord();
         }
         #endregion
 
 
         #region Methods
-        /// <summary>
-        /// 
-        /// </summary>
         void CreateRecord()
         {
             try
@@ -96,7 +77,6 @@ namespace ERPin.Modules.Cash
                 cashAcc.UpdatedDate = DateTime.Now;
                 _cashAccRepository.Add(cashAcc);
                 _unitOfWork.SaveChanges();
-                ClearRecord();
             }
             catch (Exception e)
             {
@@ -104,55 +84,6 @@ namespace ERPin.Modules.Cash
             }
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        void UpdateRecord()
-        {
-            try
-            {
-                CashAcc cashAcc = _cashAccRepository.GetById(_selectionId);
-                cashAcc.CashAccCode = txtCashAccCode.Text;
-                cashAcc.CashAccName = txtCashAccName.Text;
-                cashAcc.Description = txtDescription.Text;
-                // Audit trail
-                cashAcc.CreatedUserId = MainForm.UserId;
-                cashAcc.CreatedDate = DateTime.Now;
-                cashAcc.UpdatedUserId = MainForm.UserId;
-                cashAcc.UpdatedDate = DateTime.Now;
-                
-                _cashAccRepository.Update(cashAcc);
-                _unitOfWork.SaveChanges();
-                _messages.Update();
-                ClearRecord();
-            }
-            catch (Exception e)
-            {
-                _messages.Error(e);
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        void DeleteRecord()
-        {
-            try
-            {
-                _cashAccRepository.Delete(_selectionId);
-                _unitOfWork.SaveChanges();
-                ClearRecord();
-            }
-            catch (Exception e)
-            {
-                _messages.Error(e);
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
         void ClearRecord()
         {
             IEnumerable<TextEdit> list = groupControlCashAccInfo.Controls.OfType<TextEdit>();
@@ -163,35 +94,7 @@ namespace ERPin.Modules.Cash
 
             _edit = false;
             _selectionId = -1;
-            ListRecord();
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        void SelectRecord()
-        {
-            try
-            {
-                _edit = true;
-                _selectionId = int.Parse(gridView1.GetFocusedRowCellValue("Id").ToString());
-                txtCashAccCode.Text = gridView1.GetFocusedRowCellValue("CashAccCode").ToString();
-                txtCashAccName.Text = gridView1.GetFocusedRowCellValue("CashAccName").ToString();
-                txtDescription.Text = gridView1.GetFocusedRowCellValue("Description").ToString();
-            }
-            catch (Exception)
-            {
-                _edit = false;
-                _selectionId = -1;
-            }
-        }
-
-        void ListRecord()
-        {
-            var list = _cashAccRepository.GetAll().ToList();
-            gcList.DataSource = list;
-        }
-
         #endregion
     }
 }
